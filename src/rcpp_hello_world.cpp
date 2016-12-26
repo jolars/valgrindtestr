5877d65f54c12c71f71a696039bd93d90ec0a076
Johan Larsson <johanlarsson@outlook.com>
2016-12-26 10:56:51 +0100

    Document the function.

--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -4,6 +4,18 @@
 
 using namespace Rcpp;
 
+
+//' Red flag
+//'
+//' This function introduces a memory leak that is supposed to trigger
+//' valgrind and throw and error in R CMD check.
+//'
+//' @return Stuff.
+//'
+//' @examples
+//' repeat(ouch, 10)
+//'
+//' @export
 // [[Rcpp::export]]
 NumericVector ouch() {
   NumericVector x(10);
